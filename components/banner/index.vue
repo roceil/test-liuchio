566a2eb314cc4e25d18faaed822dccb9d3d1dc85
roceil <a0978006326@gmail.com>
2024-07-17 15:00:46 +0800

    feat(*): test

--- components/banner/index.vue
+++ components/banner/index.vue
@@ -6,16 +6,16 @@
   <div class="container flex h-[calc(100dvh-143.48px)] w-full items-center justify-center bg-white">
     <div class="flex items-center justify-center space-x-[83px]">
       <!-- TODO:之後要換成 clip-path -->
-      <img
+      <NuxtImg
         src="/banner/main-bg.png"
         class="h-[645px] w-[796px]"
-      >
+      />
 
       <!-- TODO:要討論字體問題 -->
-      <img
+      <NuxtImg
         src="/banner/main-text.svg"
         class="h-[374px] w-[170px]"
-      >
+      />
     </div>
   </div>
 
